3803740b4a893b8e8ff16fecfb88b11ea0f1de54
Samarpan Harit <haritsamarpan7@gmail.com>
2022-07-20 16:57:30 +0530

    Added colors in talwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#0A1B52',
+        input: {
+          bg: {
+            DEFAULT: "#FAF0E9",
+            disabled: "#c9c5c1"
+          }
+
+        }
+      },
+      accent: {
+        2: "#F0406D",
+      },
+      success: "#068E33",
+      failure: "#ff3333",
+      ...colors
+    }
   },
   plugins: [],
 }
