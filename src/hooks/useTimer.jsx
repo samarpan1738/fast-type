436ef9f418c3db7e4129939c8ced5e51794733f7
Samarpan Harit <haritsamarpan7@gmail.com>
2022-07-20 16:56:54 +0530

    Refactor useTimer

--- src/hooks/useTimer.jsx
+++ src/hooks/useTimer.jsx
@@ -1,63 +1,68 @@
 import React, { useEffect, useRef, useState } from 'react'
-
+const TIMER_STATE = {
+	STOPPED: "stopped",
+	STARTED: "started"
+}
 function useTimer() {
-    const [time, setTime] = useState(0);
-    const seconds = Math.floor(time / 1000);
-    const milliSeconds = time % 1000;
-    const updateTimeIntervalRef = useRef(null);
-    // 2 states = started and stopped
-    const [timerState, setTimerState] = useState("stopped");
-    
-    useEffect(() => {
-        if (timerState === "started" && updateTimeIntervalRef.current === null) {
-            // Setting updateTimeIntervalRef
-            startTimer();
-        }
-    }, [time, timerState])
+	const [time, setTime] = useState(0);
+	const seconds = Math.floor(time / 1000);
+	const milliSeconds = time % 1000;
+	const updateTimeIntervalRef = useRef(null);
+	// 2 states = started and stopped
+	const [timerState, setTimerState] = useState(TIMER_STATE.STOPPED);
 
-    useEffect(() => {
-        return () => {
-            console.log("Unmounting useTimer : ", updateTimeIntervalRef.current)
-            if (updateTimeIntervalRef.current !== null) {
-                clearInterval(updateTimeIntervalRef.current);
-                updateTimeIntervalRef.current = null;
-            }
-        }
-    }, [])
-    const addToTimer = (timeInMs) => {
-        setTime(time => time + timeInMs);
-    }
-    const resetTimer = () => {
-        setTime(0);
-        stopTimer();
-    }
-    const stopTimer = () => {
-        if (updateTimeIntervalRef.current === null) return;
-        console.log("Stopping timer")
-        clearInterval(updateTimeIntervalRef.current)
-        updateTimeIntervalRef.current = null;
-        setTimerState("stopped")
-    }
-    const startTimer = () => {
-        if (updateTimeIntervalRef.current !== null) return;
+	useEffect(() => {
+		if (timerState === TIMER_STATE.STARTED && updateTimeIntervalRef.current === null) {
+			// Setting updateTimeIntervalRef
+			startTimer();
+		}
+	}, [time, timerState])
 
-        console.log("Starting timer")
-        updateTimeIntervalRef.current = setInterval(() => {
-            setTime(time => time + 10);
-        }, 10)
-        setTimerState("started")
-    }
-    return {
-        time: {
-            seconds,
-            milliSeconds
-        },
-        timerState,
-        addToTimer,
-        resetTimer,
-        stopTimer,
-        startTimer
-    }
+	useEffect(() => {
+		return () => {
+			console.log("Unmounting useTimer : ", updateTimeIntervalRef.current)
+			if (updateTimeIntervalRef.current !== null) {
+				clearInterval(updateTimeIntervalRef.current);
+				updateTimeIntervalRef.current = null;
+			}
+		}
+	}, [])
+	const addToTimer = (timeInMs) => {
+		console.log(`Adding ${timeInMs} ms to time : ${time}`)
+		setTime(time => time + timeInMs);
+	}
+	const resetTimer = () => {
+		setTime(0);
+		stopTimer();
+	}
+	const stopTimer = () => {
+		if (updateTimeIntervalRef.current === null) return;
+		console.log("Stopping timer")
+		clearInterval(updateTimeIntervalRef.current)
+		updateTimeIntervalRef.current = null;
+		setTimerState(TIMER_STATE.STOPPED)
+	}
+	const startTimer = () => {
+		if (updateTimeIntervalRef.current !== null) return;
+
+		console.log("Starting timer")
+		updateTimeIntervalRef.current = setInterval(() => {
+			setTime(time => time + 10);
+		}, 10)
+		setTimerState(TIMER_STATE.STARTED)
+	}
+	return {
+		time: {
+			seconds,
+			milliSeconds,
+			total: time
+		},
+		timerState,
+		addToTimer,
+		resetTimer,
+		stopTimer,
+		startTimer
+	}
 }
 
 export default useTimer
